0c0f6382d293104b89a9ccb70c5fad155d6ea174
fernagonzalez <fernandogonzalez393@gmail.com>
2018-11-13 08:54:44 -0300

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ float SOC_Ultimo;
 // Adquisicion_Preliminar_I();
 // SOC_Inicial();
 // Modo_Carga();
+//   |---Actualizacion_Umbral_Maximo_Tension();
 
 // Suma de delays
 // 1000 ms en todas los modos
@@ -85,7 +86,7 @@ void loop()
     Serial.println("---------");
     delay(1000);
 
-    //Modo_Carga();
+    void Modo_Carga();
   }
 }
 
@@ -126,4 +127,60 @@ float SOC_Inicial(void)
     float SOC_Inicial = 1.0e+09 * (-0.0010*pow(x,5) + 0.0131*pow(x,4) - 0.1150*pow(x,3) + 0.6313*pow(x,2) - 1.9766*x + 2.7039);
 
     return (SOC_Inicial);
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void Modo_Carga (void)
+{
+    Serial.println("Calculando SOC");
+    if (V_bateria > Umbral_Positivo)   // Recalibración del estado de carga lleno
+      {
+      Actualizacion_Umbral_Maximo_Tension();// Salir del bucle
+      } else
+      {
+    // Determinacion del SOC con Coulumb Counting
+    Periodo_Array_Corriente_Subrutina = 350;
+      Serial.println("check 1 ");
+
+    Array_Corriente_Subrutina ();
+    // Imprimo el array de corrientes
+    //*****eliminar esta impresion en el scketch final
+    Serial.println("Los valores de las muestras de las corrientes son: ");
+    int i = 0;
+      for (i = 0; i <= 9; i++)
+      {
+       Serial.print(Array_Corriente[i]);
+       Serial.print("mA  ");
+       delay(100);
+      }
+      Serial.println(" ");
+      Serial.println("---------");
+
+     Array_Trapecios();
+    /*Serial.println("El valor del área total es: ");
+      Serial.print(Delta_Q);
+      Serial.println(" C");
+
+      Serial.print(" ");
+      Serial.println("---------");*/
+
+    /* En este modo el contador de coulumbs se presenta por Q_ganado, expresado en la siguite fórmula
+      que representa la cantidad de cargas acumulada en T */
+    Q_ganado += Delta_Q;
+
+    float Q_nominal = 9360; //Capacidad nominal (en Coulumb)
+    /* La capacidad nominal de la batería se calcula teniendo en cuenta la capacidad nominal en mAh,
+        de la hoja de datos tenemos que la capadidad es 2.6 Ah para una sola bateria,
+        pero como están conectadas en serie, el pack de tres baterias tiene la misma capacidad.
+        Teniendo en cuenta la relación entre amper y coulumbs hacemos: 2.6 Ah = 2.6 (C/s)*3600 s =9360 C
+    */
+
+    float Delta_SOC = 100 * Q_ganado / Q_nominal;
+    SOC += Delta_SOC;
+    SOC_Ultimo = SOC;
+
+    Valor_SOC_Coulumb_Counting();
+    }
+  return 0;
 }
